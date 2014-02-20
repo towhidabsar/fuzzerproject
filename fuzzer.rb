788f66ff13381e567d3e66e7ea48c31c25420086
SidIcarus <app6141@gmail.com>
2014-02-20 11:19:36 -0800

    Fixed OpenSSL certificate error.

--- fuzzer.rb
+++ fuzzer.rb
@@ -6,31 +6,39 @@ visitedLinks = Array.new
 
 def linkDiscover( url )
 #create a new Mechanize agent for crawling
-	agent = Mechanize.new
+	agent = Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', 
+	OpenSSL::SSL::VERIFY_NONE}
 
 	puts url
 
 	page = agent.get(url)
-	listLinks.push(page.links)
+	listLinks = page.links
+	visitedLinks = []
 	
 	listLinks.each do |link|
-		visitedLinks.push(page)
-		puts link.uri
+		visitedLinks << page
+		#puts link.uri
+		puts page.links
 		page = agent.get(link.uri)
-		listLinks.push(page.links)
+		listLinks << page.links
 	end
-
 end
 
-puts "Welcome to Fuzz Web Application Testing Tool. Please enter - fuzz [discover | test] url OPTIONS"
+puts "Welcome to Fuzzy, the Web Application Testing Tool."
+puts "Please enter: fuzz [discover | test] <url> OPTIONS"
 
 while true
 	command = gets.chomp
 	command = command.split
+
 	if command[0] == "fuzz"
 		case command[1]
 			when /\Adiscover\z/i
 				linkDiscover( command[2] )
 		end
+		#case command[1]
+		#	when /\Atest\z/i
+				#do something here, le hue
+		#end
 	end
 end
