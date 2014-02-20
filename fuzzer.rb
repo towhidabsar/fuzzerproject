d01ac301721768afba43bfda4aa3dd7396d5a5b0
SidIcarus <app6141@gmail.com>
2014-02-20 14:34:03 -0800

    Created base for functions.

--- fuzzer.rb
+++ fuzzer.rb
@@ -1,8 +1,9 @@
 require 'mechanize'
 require 'rubygems'
 
-listLinks = Array.new
-visitedLinks = Array.new
+$listLinks = Array.new
+$visitedLinks = Array.new
+$linksFilter
 
 def linkDiscover( url )
 #create a new Mechanize agent for crawling
@@ -12,33 +13,57 @@ def linkDiscover( url )
 	puts url
 
 	page = agent.get(url)
-	listLinks = page.links
-	visitedLinks = []
-	
-	listLinks.each do |link|
-		visitedLinks << page
-		#puts link.uri
+	$listLinks = page.links
+	$visitedLinks = []
+	puts [].length
+	puts $listLinks.length
+
+	$listLinks.each do |link|
+		$visitedLinks << page
+		puts link.uri
 		puts page.links
 		page = agent.get(link.uri)
-		listLinks << page.links
+		$listLinks << page.links
+
+
 	end
 end
 
-puts "Welcome to Fuzzy, the Web Application Testing Tool."
-puts "Please enter: fuzz [discover | test] <url> OPTIONS"
+#Remove links that go offsite from given array into corresponding array
+def linkOffsiteFilter
 
-while true
-	command = gets.chomp
-	command = command.split
+end
 
-	if command[0] == "fuzz"
-		case command[1]
-			when /\Adiscover\z/i
-				linkDiscover( command[2] )
+#Creates the arrays that are needed to hold the links
+def createLinksArrs
+
+end
+
+
+def main
+	puts "Welcome to Fuzzy, the Web Applicadtion Testing Tool."
+	puts "Please enter: fuzz [discover | test] <url> OPTIONS"
+
+	while true
+		command = gets.chomp
+		command = command.split
+
+		if command[0] == "fuzz"
+			case command[1]
+				when /\Adiscover\z/i
+					linkDiscover( command[2] )
+			end
+			#case command[1]
+			#	when /\Atest\z/i
+					#do something here, le hue
+			#end
+		else 
+			puts command[0]+ " is an invalid command."
 		end
-		#case command[1]
-		#	when /\Atest\z/i
-				#do something here, le hue
-		#end
 	end
 end
+
+main
+
+#.select or .findall
+# .select takes in a block
