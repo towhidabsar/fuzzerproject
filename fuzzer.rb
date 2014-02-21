9f86a7aba2c4084b1c61a4e0ed55a497f213a507
Md Towhidul Chowdhury <mac9908@rit.edu>
2014-02-20 20:58:50 -0500

    Working on displaying all the inputs in a human readable way.

--- fuzzer.rb
+++ fuzzer.rb
@@ -16,6 +16,7 @@ def inputDiscover(page)
 	url = url.split('?')
 	puts "AA"
 	link = url[0]
+	puts link
 	input = url[1]
 	if not $filteredInputs.has_key? link
 		$filteredInputs[link] = Array.new
@@ -49,15 +50,19 @@ def linkDiscover( url )
 	$listLinks.each do |link|
 		if not linkOffsiteFilter(link)
 			if not $visitedLinks.include? link
-				$visitedLinks << link
-				currentPage = agent.get(link)
-				puts currentPage.link
-				inputDiscover(currentPage)
-				currentPage.links.each do |link|
-					$listLinks << link.uri
+				begin
+					$visitedLinks << link
+					currentPage = agent.get(link)
+					puts currentPage.link
+					inputDiscover(currentPage) 
+					currentPage.links.each do |link|
+						$listLinks << link.uri
+					end
+					#Make sure no duplicate links are present just in case
+					$listLinks = $listLinks.uniq
+				rescue
+					puts "There was an exception. It was ignored"
 				end
-				#Make sure no duplicate links are present just in case
-				$listLinks = $listLinks.uniq
 			end
 		end
 	end
@@ -72,8 +77,15 @@ def linkOffsiteFilter(link)
 end
 
 #Creates the arrays that are needed to hold the links
-def createLinksArrs
-
+def displayInputs
+	$filteredInputs.each do |link, array|
+		puts "For <"+link+"> : Inputs"
+		puts "#############################################################################"
+		array.each do |input|
+			puts input
+		end
+		puts "#############################################################################"
+	end
 end
 
 
@@ -88,7 +100,13 @@ def main
 		if command[0] == "fuzz"
 			case command[1]
 				when /\Adiscover\z/i
+					$listLinks = Array.new
+					$visitedLinks = Array.new
+					$filteredInputs = Hash.new
+					$domain = ""
 					linkDiscover( command[2] )
+					puts "Displaying all the inputs of the system"
+					displayInputs
 			end
 			#case command[1]
 			#	when /\Atest\z/i
