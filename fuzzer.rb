39a0032cb3563e20aaf8d0fba7b5c0e96057b62a
SidIcarus <app6141@gmail.com>
2014-02-21 17:26:23 -0800

    Moved a method to another file.

--- fuzzer.rb
+++ fuzzer.rb
@@ -1,36 +1,16 @@
 require 'mechanize'
 require 'rubygems'
 require 'uri'
-
+require InputValidation.rb
+require InputDiscovery.rb
+require CustomAuthentication.rb
+ 
 $listLinks = Array.new
 $visitedLinks = Array.new
 $filteredInputs = Hash.new
 $domain = ""
 $linksFilter
 
-#Discovers all the inputs in the page.
-def inputDiscover(page)
-	#Get the URL of the current page.
-	url = page.link.uri
-	url = url.to_s
-	url = url.split('?')
-	puts "AA"
-	link = url[0]
-	puts link
-	input = url[1]
-	if not $filteredInputs.has_key? link
-		$filteredInputs[link] = Array.new
-	end
-	if url.length > 1
-		input = input.split('&')
-		input.each do |x|
-			$filteredInputs[link] << x
-		end
-	end
-	#Get all the form parameters of the page
-	inputForms = page.forms
-	$filteredInputs[link] << inputForms
-end
 
 def linkDiscover( url )
 	agent = Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', 
