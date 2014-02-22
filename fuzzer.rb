d8f6e599f92d3156ac2695a2de30197897b317fd
SidIcarus <app6141@gmail.com>
2014-02-21 17:56:57 -0800

    Git dun goof'd

--- fuzzer.rb
+++ fuzzer.rb
@@ -71,40 +71,7 @@ def displayInputs
 	end
 end
 
-#
-def main
-	puts "Welcome to Fuzzy, the Web Applicadtion Testing Tool."
-	puts "Please enter: fuzz [discover | test] <url> OPTIONS"
-	puts "/tMore Options: -- "
-	puts "/t/tcustom-auth="
-
-	while true
-		command = gets.chomp
-		command = command.split
-
-		if command[0] == "fuzz"
-			case command[1]
-				when /\Adiscover\z/i
-					$listLinks = Array.new
-					$visitedLinks = Array.new
-					$filteredInputs = Hash.new
-					$domain = ""
-					linkDiscover( command[2] )
-					puts "Displaying all the inputs of the system"
-					displayInputs
-			end
-			#case command[1]
-			#	when /\Atest\z/i
-					#do something here, le hue
-			#end
-		else 
-			puts " An invalid command."
-		end
-	end
-end
-
-main
-puts "THE END"
-
 #.select or .findall
-# .select takes in a block
+# .select takes in a block 
+
+# this is a useless comment, please delete me
