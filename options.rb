479274cc2832f6c462b1ef80b6accb5c0cbfc70c
Md Towhidul Chowdhury <mac9908@rit.edu>
2014-03-16 01:53:54 -0400

    Modified the entire package to implement command line options which work well. Need to modify the vectors.rb so that instead of the entire array it gives out one single vector at a time. Seeing light at the end of the dark tunnel.

--- options.rb
+++ options.rb
@@ -6,11 +6,15 @@ require_relative 'inputDiscovery'
 require_relative 'customAuthentication'
 require_relative 'displayResults'
 require_relative 'pageDiscovery'
+require_relative 'vectors'
 
 class Options
 
-	def self.fuzzDiscover(agent, mainURL)
-	
+	def self.fuzzDiscover(agent, mainURL, customAuth)
+		
+		#Authenticate using the custom authentication string
+		CustomAuthentication.authenticate(agent, mainURL, customAuth)
+		
 		#Initialize the required variables
 		linkQueries = Hash.new
 		formInputs = Hash.new
@@ -24,7 +28,8 @@ class Options
 			curPage = agent.get(link)
 			
 			# Get all the queries in the link
-			linkQueries = InputDiscovery.discoverQueries(curPage, linkQueries)
+			linkQueries = InputDiscovery.discoverQueries(curPage, 
+										linkQueries)
 			
 			# Get all the input forms in the page
 			formInputs = InputDiscovery.discoverForms(curPage, formInputs)
@@ -35,60 +40,101 @@ class Options
 		return [foundLinks, linkQueries, formInputs, cookies] 
 	end
 	
-	def self.fuzzTest(agent, mainURL) 
-		results = fuzzDiscover( agent, mainURL)
+	def self.fuzzTest(agent, mainURL, cmdLineOptions) 
+		#Get all the required command line options
+		customAuth = cmdLineOptions[0]
+		vectorFile = cmdLineOptions[1]
+		sensitiveFile = cmdLineOptions[2]
+		random = cmdLineOptions[3]
+		slow = cmdLineOptions[4]
+		
+		#Create the vector object that contains the list of all the
+		#vectors to be fuzzed.
+		vectors = Vector.new(vectorFile)
+		
+		#Run fuzz discover first, get all the results.
+		results = fuzzDiscover( agent, mainURL, customAuth)
 		linkQueries = results[1]
 		formInputs = results[2]
 		cookies = results[3]
- 
-     curSecurity = ["low", "medium", "high"]
-     i = 0
-     #For dvwa change cookie value, start with low, medium and then high.
+		
+		#This is custom for dvwa. It has been hard coded temporarily.
+		#It will still work for other websites, but just go through
+		#the same loop 3 times.
+		curSecurity = ["low", "medium", "high"]
+		i = 0
+		
+		#For dvwa change cookie value, start with low, medium and then high.
 		while i<3
 			cookies.each do |cookie|
 				if cookie.name == "security"
 					 cookie.value = curSecurity[i]
-					 puts "################### SECURITY LEVEL : #{curSecurity[i]} ###################"
+					 puts "################### SECURITY LEVEL :
+					 #{curSecurity[i]} ###################"
 				end
 			end
 			puts "############# LINK QUERIES ######################"
-			fuzzLinkQueries(agent, linkQueries, '<script>alert("XSS")</script>')
+			fuzzLinkQueries(agent, linkQueries, vectors)
 			puts "############# FORM INPUTS ######################"
-			fuzzFormInputs(agent, formInputs, '<script>alert("XSS")</script>')
+			fuzzFormInputs(agent, formInputs, vectors)
 			i += 1
 		end
 	end
 	
-	# Check all the link inputs with fuzz vectors.
-	def self.fuzzLinkQueries(agent, linkQueries, vector)	
-		linkQueries.each_key do |link|
-			puts link
-			agent.post( link, vector) 		
-			start_time = Time.now
-			checkSanitization( agent.page, vector)
-			wait_time = Time.now - start_time
-		end
-	end
-	# Check all the form inputs
-	def self.fuzzFormInputs(agent, formInputs, vector)
-		formInputs.each_key do |link|
-			puts link
-			page = agent.get( link )
-			form = page.forms.first
-			if form != nil
-				form.fields_with( :value => "").each do |field|
-					field.value = vector
-				end
-				button = form.button_with( :value => /submit/)
-				agent.submit( form, button)
+	# Check all the link queries with fuzz vectors.
+	def self.fuzzLinkQueries(agent, linkQueries, vectorList)
+		#Check if there is a vector next in the vectorList
+		while vectorList.has_next?
+			#Get the next vector
+			vector = vectorList.next_fuzzVector
+			#Go through the list of queries with one specific vector
+			linkQueries.each_key do |link|
+				puts link
+				agent.post( link, vector) 		
 				start_time = Time.now
 				checkSanitization( agent.page, vector)
 				wait_time = Time.now - start_time
-				
 			end
 		end
+		#After parsing reset the vector count.
+		vectorList.reset_count!
 	end
 	
+	# Check all the form inputs with fuzz vectors.
+	def self.fuzzFormInputs(agent, formInputs, vectorList)
+		#Check if there is a vector next in the vectorList
+		while vectorList.has_next?
+			#Get the next vector
+			vector = vectorList.next_fuzzVector
+			#Go through the list of inputs with one specific vector
+			formInputs.each_key do |link|
+				puts link
+				page = agent.get( link )
+				form = page.forms.first
+				if form != nil
+					form.fields_with( :value => "").each do |field|
+						field.value = vector
+					end
+					button = form.button_with( :value => /submit/)
+					agent.submit( form, button)
+					start_time = Time.now
+					checkSanitization( agent.page, vector)
+					wait_time = Time.now - start_time
+				end
+			end
+		end
+		#After parsing reset the vector count.
+		vectorList.reset_count!	
+	end
+	
+	
+
+=begin
+	This part deals with all the method responsible for checking
+	if the output meets the required validation properties such
+	as sanitized input, response time limits, sensitive data
+	not leaked etc.
+=end
 	def self.checkSanitization( page, vector)
 		if page.body.include?(vector.chomp)
 			puts "NO SANITIZATION"
