1b23ba7b41ebfdeac2d442ac7671401456a8eb04
SidIcarus <app6141@gmail.com>
2014-03-15 20:16:15 -0500

    Commented out unused code

--- vectors.rb
+++ vectors.rb
@@ -1,14 +1,13 @@
 class Vector
 
-  attr_reader :escape_sequences, :xss, :active_sql
-
   def initialize file
-    @fuzzVector = File.readlines(resource_path(file)).map{|line| line.strip}
+   #@fuzzVector = File.readlines(resource_path(file)).map{|line| line.strip}
+   @fuzzVector = File.readlines(file).map{|line| line.strip}
   end
 
-  def resource_path file
-    "resources/vectors/" + file
-  end
+  #def resource_path file
+  #  "resources/vectors/" + file
+  #end
 
   def next_fuzzVector
     attack = @fuzzVector
