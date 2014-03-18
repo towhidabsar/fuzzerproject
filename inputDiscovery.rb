1782c5041ddeaea4b9683b5bdc222051e6f43cf4
Sid <sid@Sid.(none)>
2014-03-18 15:46:50 -0400

    Removed redundancy and added in comments

--- inputDiscovery.rb
+++ inputDiscovery.rb
@@ -1,7 +1,3 @@
-require 'mechanize'
-require 'rubygems'
-require 'uri'
-
 # Discovers all the inputs in the page
 module InputDiscovery
 
@@ -10,27 +6,22 @@ module InputDiscovery
 	# +page+:: The +Page+ which to crawl for queries
 	# +linkQueries+:: +Hash+ object of [link, queries]
 	# Return: Updated +linkInputs+ with queries from the given +page+
-	def discoverQueries(page)
-		
+	def discoverQueries
 		# All the queries from the given page
 		pageQueries = page.uri.query
 
-		# The uri of the page
-		uri = page.uri # i.e. 'localhost/a/efefefwfqwe'
-
 		# Check to see if given host is in the hash map
-		if not linkQueries.has_key? uri
-			linkQueries[uri] = Array.new
+		if not @linkQueries.has_key? page.uri
+			@linkQueries[page.uri] = Array.new
 		end
 
 		if not pageQueries == nil
 			pageQueries = pageQueries.split("&")
 			# Traverse each query in pageQueries & append to it's array
 			pageQueries.each do |query|
-				linkQueries[uri] << query
+				@linkQueries[page.uri] << query
 			end
 		end
-		return linkQueries
 	end 
 
 	# Parses the given list of forms & add them to the given hash map
@@ -38,26 +29,17 @@ module InputDiscovery
 	# +page+:: Acquires the forms and +URI+
 	# +formInputs+:: +Hash+ object of [link, queries]
 	# Return: Updated +formInputs+ with forms from the given +page+
-	def discoverForms(page)
-
-		# Get all the input forms in the page.
-		pageForms = page.forms
-
-		# The uri of the page
-		uri = page.uri # i.e. 'localhost/a/efefefwfqwe'
-
+	def discoverForms
 		# Check to see if given host is in the hash map
-		if not formInputs.has_key? uri
-			formInputs[uri] = Array.new
+		if not @formInputs.has_key? page.uri
+			@formInputs[page.uri] = Array.new
 		end
 		
-		# Traverse each form in pageForms & add it to it's array
-		@pageForms.each do |form|
-			formInputs[uri].concat(form.fields)
-			formInputs[uri].concat(form.buttons)
+		# Traverse each form in page.forms & add it to it's array
+		page.forms.each do |form|
+			@formInputs[page.uri].concat(form.fields)
+			@formInputs[page.uri].concat(form.buttons)
 		end
-		
-		return formInputs
 	end
 
 	# Acquires the browsers cookies via the given +agent+ & adds it to
--- vectors.rb
+++ vectors.rb
@@ -1,31 +1,31 @@
 class Vector
 
   def initialize file
-   #@fuzzVector = File.readlines(resource_path(file)).map{|line| line.strip}
-   @fuzzVector = File.readlines(file).map{|line| line.strip}
-   @length = @fuzzVector.length
-   @count = 0
+    #@fuzzVector = File.readlines(resource_path(file)).map{|line| line.strip}
+    @fuzzVector = File.readlines(file).map{|line| line.strip}
+    @length = @fuzzVector.length
+    @count = 0
   end
 
   #def resource_path file
   #  "resources/vectors/" + file
   #end
 
-  #Give the current fuzzVector and rotate to the next one.
+  # Give the current fuzzVector and rotate to the next one.
   def next_fuzzVector
     attack = @fuzzVector
     @fuzzVector.rotate!
-	@count += 1
+    @count += 1
     return attack[0]
   end
-  
-  #Reset the count so that has_next? works multiple times.
+
+  # Reset the count so that has_next? works multiple times.
   def reset_count!
-	@count = 0
+    @count = 0
   end
-  
-  #Checks whether the entire vecotr list has been traversed.
+
+  # Checks whether the entire vecotr list has been traversed.
   def has_next?
-	return @count != @length
+    return @count != @length
   end
 end
